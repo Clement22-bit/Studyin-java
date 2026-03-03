4f2c355f3b6d2a69773f466d982a5fdad1d1aaef
Clemente <pedronhinho@outlook.com.br>
2026-03-02 22:24:18 -0300

    exercicio 01

---
+++ exercicio01/MeuPerfil.java
@@ -0,0 +1,19 @@
+package exercicio01;
+
+public class MeuPerfil {
+    public static void main(String[] args) {
+        String nome = "Pedro Henrique";
+        int idade = 21;
+        String cidade = "pindamonhangaba";
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Cidade: " + cidade);
+    }
+}
+
+    
+
+
+        
+    
